f089c39a2111dd38f8d754ddc1bd35bb2ce15149
Joao Tinti <joaotinti75@gmail.com>
2021-01-09 17:54:48 -0300

    implementando uma callback function

--- among-us-minigame/script.js
+++ among-us-minigame/script.js
@@ -1,16 +1,19 @@
 let botoes = document.querySelectorAll('.botoes')
 let luzes = document.querySelectorAll('.luzes .luz')
-let index_luz = 0
-let listaNumAleatorio = []
+let botoesJogador = document.querySelectorAll('.botoes-jogador')
+let luzesJogador = document.querySelectorAll('.luzes-jogador .luz')
+let listaNumAleatorioCopy = []
 
 function executarComputador(nivel){
+    let listaNumAleatorio = []
+    let indexLuz = 0
     let interval = setInterval(() => {
         if (listaNumAleatorio.length === nivel){
             console.log('terminou')
+            listaNumAleatorioCopy = listaNumAleatorio
             listaNumAleatorio.forEach((num) => {
                 botoes[num].style.backgroundColor = '#000'
             })
-            //console.log(listaNumAleatorio)
             clearInterval(interval)
         } else {
             let numAleatorio = Math.floor(Math.random() * botoes.length)  //varia de 0 a 8
@@ -19,12 +22,37 @@ function executarComputador(nivel){
             })
             listaNumAleatorio.push(numAleatorio)
             botoes[numAleatorio].style.backgroundColor = '#39a4d2'
-            luzes[index_luz].style.backgroundColor = '#12e012'
-            ++index_luz
+            luzes[indexLuz].style.backgroundColor = '#12e012'
+            ++indexLuz
             console.log('ola mundo')
         } 
     }, 1000)
+
 }
 
-executarComputador(1)
+function executarJogador(nivel, listaNumeros){
+    let indexLuzJogador = 0
+    console.log(listaNumeros)
+    alert('ola mundo')
+    botoesJogador.forEach((botao) => {
+        botao.addEventListener('click', () => {
+            botoesJogador.forEach((btn) => {
+                btn.style.backgroundColor = '#ddd'
+            })
+            
+            botao.style.backgroundColor = '#39a4d2'
+            luzesJogador[indexLuzJogador].style.backgroundColor = '#12e012'
+            
+            if (indexLuzJogador === nivel) {
+                return
+            } else {
+                ++indexLuzJogador
+            }
+                        
+        })
+    })
+}
+
+executarComputador(3)
+setTimeout(executarJogador(3-1, listaNumAleatorioCopy), 3000)
 
