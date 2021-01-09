95b71e44e573f1a4342829345f88fe411b9cd2de
Joao Tinti <joaotinti75@gmail.com>
2021-01-09 12:33:33 -0300

    Comecando a implementar a logica de apagar o retangulo

--- among-us-minigame/script.js
+++ among-us-minigame/script.js
@@ -3,31 +3,27 @@ let luzes = document.querySelectorAll('.luzes .luz')
 let index_luz = 0
 let listaNumAleatorio = []
 
-function sorteiaNumero(){
-    let numAleatorio = Math.floor(Math.random() * botoes.length)  //varia de 0 a 8
-    if (listaNumAleatorio.indexOf(numAleatorio) === -1) {
-        listaNumAleatorio.push(numAleatorio)
-    } else {
-        while (listaNumAleatorio.indexOf(numAleatorio) != -1) {
-            let numAleatorio = Math.floor(Math.random() * botoes.length)
-        }
-        listaNumAleatorio.push(numAleatorio)
-    }
-
-    return numAleatorio
-}
 
 let interval = setInterval(() => {
-        var numAleatorio = Math.floor(Math.random() * botoes.length)  //varia de 0 a 8
-        listaNumAleatorio.push(numAleatorio)
-        botoes[numAleatorio].style.backgroundColor = '#39a4d2'
-        luzes[index_luz].style.backgroundColor = '#12e012'
+        if (listaNumAleatorio.length === 3){
+            console.log('terminou')
+            console.log(listaNumAleatorio)
+            clearInterval(interval)
+        } else {
+            var numAleatorio = Math.floor(Math.random() * botoes.length)  //varia de 0 a 8
+            listaNumAleatorio.push(numAleatorio)
+            botoes[numAleatorio].style.backgroundColor = '#39a4d2'
+            luzes[index_luz].style.backgroundColor = '#12e012'
+            console.log('ola mundo')
+        } 
     }, 1000)
 
-    setTimeout(() => {
-    listaNumAleatorio.forEach((num) => {
-        botoes[num].style.backgroundColor = '#000'
-    })
-    listaNumAleatorio = []
-    clearInterval(interval)
-}, 5000) //A cada nivel de dificultade, eu aumento o tempo de parada em mais 1000ms
+/*
+setTimeout(() => {
+        listaNumAleatorio.forEach((num) => {
+            botoes[num].style.backgroundColor = '#000'
+        })
+        listaNumAleatorio = []
+        clearInterval(interval)
+}, 3000) //A cada nivel de dificultade, eu aumento o tempo de parada em mais 1000ms
+*/
