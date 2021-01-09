2856c1590c90d20fb8433377699a5e4c183cc774
Joao Tinti <joaotinti75@gmail.com>
2021-01-09 13:06:16 -0300

    Comecando a adicionar a entrada do usuario

--- among-us-minigame/script.js
+++ among-us-minigame/script.js
@@ -3,10 +3,13 @@ let luzes = document.querySelectorAll('.luzes .luz')
 let index_luz = 0
 let listaNumAleatorio = []
 
-
-let interval = setInterval(() => {
-        if (listaNumAleatorio.length === 3){
+function executarComputador(nivel){
+    let interval = setInterval(() => {
+        if (listaNumAleatorio.length === nivel){
             console.log('terminou')
+            listaNumAleatorio.forEach((num) => {
+                botoes[num].style.backgroundColor = '#000'
+            })
             //console.log(listaNumAleatorio)
             clearInterval(interval)
         } else {
@@ -17,16 +20,11 @@ let interval = setInterval(() => {
             listaNumAleatorio.push(numAleatorio)
             botoes[numAleatorio].style.backgroundColor = '#39a4d2'
             luzes[index_luz].style.backgroundColor = '#12e012'
+            ++index_luz
             console.log('ola mundo')
         } 
     }, 1000)
+}
 
-/*
-setTimeout(() => {
-        listaNumAleatorio.forEach((num) => {
-            botoes[num].style.backgroundColor = '#000'
-        })
-        listaNumAleatorio = []
-        clearInterval(interval)
-}, 3000) //A cada nivel de dificultade, eu aumento o tempo de parada em mais 1000ms
-*/
+executarComputador(1)
+
